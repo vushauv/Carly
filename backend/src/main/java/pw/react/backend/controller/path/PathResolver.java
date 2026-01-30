1e78fb9ed32789b22913fe523efea749513a952b
Oleh Shuptar <01193038@pw.edu.pl>
2026-01-30 17:40:20 +0100

    fix: backend - fixed pathResolver

--- backend/src/main/java/pw/react/backend/controller/path/PathResolver.java
+++ backend/src/main/java/pw/react/backend/controller/path/PathResolver.java
@@ -1,27 +1,27 @@
 package pw.react.backend.controller.path;
 
 public final class PathResolver {
-    public static final String Root = "/api/";
+    public static final String Root = "/api";
     public static class Car {
-        public static final String Base = Root + "cars";
+        public static final String Base = Root + "/cars";
         public static final String Images = "/images";
     }
 
     public static class Booking{
-        public static final String Base = Root + "bookings";
+        public static final String Base = "/bookings";
     }
 
     public static class Flatly{
-        public static final String Base = Root + "flatly";
-        public static final String Bookings = Root + "bookings";
-        public static final String Flats = Root + "flats";
-        public static final String FlatBookings = Root + "flat-bookings";
+        public static final String Base = Root + "/flatly";
+        public static final String Bookings = "/bookings";
+        public static final String Flats = "/flats";
+        public static final String FlatBookings = "/flat-bookings";
     }
 
     public static class Parkly {
-        public static final String Base = Root + "parkly";
-        public static final String Cars = Root + "cars";
-        public static final String CarBookings = Root + "car-bookings";
+        public static final String Base = Root + "/parkly";
+        public static final String Cars = "/cars";
+        public static final String CarBookings = "/car-bookings";
     }
 
     public static class Users{
